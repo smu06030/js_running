3c5ecb5ed491987b8ac77e369f67650697237c06
incheol <smu06030>
2024-07-24 14:34:36 +0900

    240724 재제출

--- 240724/5.js
+++ 240724/5.js
@@ -1,11 +1,17 @@
 const scores = [36, 62, 72, 55, 86, 95, 92, 48, 81];
 
-function plusScore(scores) {
+function calculator(scores, sum) {
   for(let i=0; i<scores.length; i++){
-    scores[i] += 3
+    scores[i] += sum
   }
 }
 
+function plusScore(scores) {
+  let sum = 3;
+
+  calculator(scores, sum)
+}
+
 plusScore(scores)
+
 console.log(scores);
-// 기대값: [39, 65, 75, 58, 89, 98, 95, 51, 84]
