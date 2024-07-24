4145395df410330f007b684f7374297989489aee
incheol <smu06030>
2024-07-24 14:47:22 +0900

    240724-3

--- 240724/5.js
+++ 240724/5.js
@@ -1,15 +1,27 @@
 const scores = [36, 62, 72, 55, 86, 95, 92, 48, 81];
 
-function calculator(scores, sum) {
-  for(let i=0; i<scores.length; i++){
-    scores[i] += sum
+function calculator(first, op, last) {
+  let result;
+  switch(op){
+    case '+' :  result = first + last;
+    break;
+    case '-' :  result = first - last;
+    break;
+    case '*' :  result = first * last;
+    break;
+    case '/' :  result = first / last;
+    break;
   }
+  
+  return result;
 }
 
 function plusScore(scores) {
   let sum = 3;
 
-  calculator(scores, sum)
+  for(let i=0; i<scores.length; i++){
+    scores[i] = calculator(scores[i], '+', sum)
+  }
 }
 
 plusScore(scores)
